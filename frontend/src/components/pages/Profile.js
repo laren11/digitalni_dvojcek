6ee262c8b34b97ca7a19737f8b4f27b28459f8db
laren11 <laren.sedlanic@gmail.com>
2023-05-25 00:48:03 +0200

    SLTS-35 Showing "Display Name" on Profile page

--- frontend/src/components/pages/Profile.js
+++ frontend/src/components/pages/Profile.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import UserContext from "../../context/userContext";
 
 const Profile = () => {
+  const { userData } = useContext(UserContext);
+  console.log("USERDATA: ", userData);
   return (
     <div>
       {" "}
       <div className="background">
         <h1 className="title">Your profile</h1>
-        <div style={{ color: "white" }}>Profile administration</div>
+        <div style={{ color: "white", margin: "3%" }}>
+          Display Name:{" "}
+          {userData?.user?.displayName && userData?.user?.displayName}
+        </div>
         <div style={{ height: "200px" }}></div>
       </div>
     </div>
