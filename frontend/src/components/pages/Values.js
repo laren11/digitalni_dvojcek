0a4520147ddfd874d3a61f4cbc6d547b9c264d5f
JakobKrajnc <jakob.krajnc@student.um.si>
2023-05-29 21:57:55 +0200

    SLTS-28 Sending crypto values to tables with sockets

--- frontend/src/components/pages/Values.js
+++ frontend/src/components/pages/Values.js
@@ -2,18 +2,32 @@ import React, { useEffect, useContext, useState } from "react";
 import TableNoExchange from "../tables/TableNoExchange";
 import { VALUECOLUMNS } from "../../constants/Columns";
 import UserContext from "../../context/userContext";
+import { socket } from "../../socket.js";
 
 const Values = () => {
   const { userData } = useContext(UserContext);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    socket.on("userCryptos", (data) => {
+      setData(data);
+    });
+
+    // Unsubscribe from the event when the component unmounts
+    return () => {
+      socket.off("userCryptos");
+    };
+  }, []);
+
+  useEffect(() => {
+    socket.emit("getUserCryptos", userData?.user.id);
+  }, []);
 
   return (
     <div className="background">
       <h1 className="title">Your values</h1>
       {userData?.user?.id && (
-        <TableNoExchange
-          columns={VALUECOLUMNS}
-          request={` http://localhost:3001/prices/getUserPrices/${userData?.user.id}`}
-        />
+        <TableNoExchange columns={VALUECOLUMNS} data={data} />
       )}
 
       <div style={{ height: "200px" }}></div>
