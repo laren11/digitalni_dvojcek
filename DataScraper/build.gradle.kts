3166d5ffc2b9a84648b1a7ed82c2b7190ccc41c0
JakobKrajnc <jakob.krajnc@student.um.si>
2023-05-31 18:32:19 +0200

    SLTS-43 Added GeoJSON transformation (needs fixing)

--- DataScraper/build.gradle.kts
+++ DataScraper/build.gradle.kts
@@ -31,6 +31,7 @@ kotlin {
                 implementation ("org.slf4j:slf4j-simple:1.7.30")
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
                 implementation("com.squareup.okhttp3:okhttp:4.9.1")
+                implementation ("com.google.code.gson:gson:2.8.9")
             }
         }
         val jvmTest by getting
